e1fdfda3a16629f3fd918521484888dcf6da3c36
d01 <my.deepesh.1.1.1@gmail.com>
2020-08-28 13:10:15 +0530

    final deployment

---
+++ precache-manifest.f3acb1132a420f6d2119c50a357fba62.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cfe81c0b2f4bf833d68125cb97b588",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e24c30fedf0ef1af52ba",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "e9e2f87e846525d47ab1",
+    "url": "./static/css/main.03b88df9.chunk.css"
+  },
+  {
+    "revision": "e24c30fedf0ef1af52ba",
+    "url": "./static/js/2.c3be621e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "./static/js/2.c3be621e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e2f87e846525d47ab1",
+    "url": "./static/js/main.2424e550.chunk.js"
+  },
+  {
+    "revision": "543081f6249ee7c58a0a",
+    "url": "./static/js/runtime-main.df5fe3a5.js"
+  },
+  {
+    "revision": "ff7dfca256abe5c409f870b5d557261b",
+    "url": "./static/media/como.ff7dfca2.png"
+  },
+  {
+    "revision": "13c5ba4af5a02d97b40670fac3ac0ce7",
+    "url": "./static/media/compppr.13c5ba4a.png"
+  },
+  {
+    "revision": "25965bc0b6d8811c210ca3e0ede969c8",
+    "url": "./static/media/comprck.25965bc0.png"
+  },
+  {
+    "revision": "fc53979dccb4046ebad9981ac6512a09",
+    "url": "./static/media/compsc.fc53979d.png"
+  },
+  {
+    "revision": "75a3bf0d8861c6982123db1666350115",
+    "url": "./static/media/paper.75a3bf0d.png"
+  },
+  {
+    "revision": "82c35f3a1e688afac4e3a2bde5274a68",
+    "url": "./static/media/plppr.82c35f3a.png"
+  },
+  {
+    "revision": "1166b59841e28e00bf25b8ba0ff33e49",
+    "url": "./static/media/plrck.1166b598.png"
+  },
+  {
+    "revision": "726c3943ec0002e48e6dfabfbbdcd4d9",
+    "url": "./static/media/plsc.726c3943.png"
+  },
+  {
+    "revision": "e59f12e32cc8cdc4570c1c9086c20287",
+    "url": "./static/media/yomo.e59f12e3.png"
+  }
+]);
